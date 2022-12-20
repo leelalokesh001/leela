ab29401347a3b7e6cb3aecd0c7bc35f9a110d554
leelalokesh001 <121089139+leelalokesh001@users.noreply.github.com>
2022-12-20 14:09:28 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class Test
+ {
+  public static void main(String args[])
+   {
+    int i;
+    for(i=0;i<10;i++)
+     {
+      System.out.println("2*"+i+"="+(2*i));
+     }
+   }
+ }
+    
